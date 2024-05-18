95dbf0c4bd1f2331d38407f41fee24d8de2bc1ec
Vsmart <Vsmart@DESKTOP-80JSJRV>
2024-05-18 19:53:23 +0530

    Updated

--- src/main/java/appiumTouchActions/appTouchActions.java
+++ src/main/java/appiumTouchActions/appTouchActions.java
@@ -8,20 +8,25 @@ import java.time.Duration;
 import java.util.Collections;
 
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Interactive;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.ImmutableList;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import wrappers.GenericWrappers;
 
-public class appTouchActions {
+public class appTouchActions extends GenericWrappers {
 	
-	static AppiumBy driver;
+	static AppiumDriver driver;
+
 
 	@Test
 	public void AmazonAppLaunch() throws MalformedURLException, URISyntaxException, Exception {
@@ -37,32 +42,27 @@ public class appTouchActions {
 		// dc.setCapability("appActivity", "com.testleaf.leaforg.MainActivity");
 
 		// instantiating the Appium Driver
-		AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
+		driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
 		driver.findElement(AppiumBy.id("com.amazon.mShop.android.shopping:id/skip_sign_in_button")).click();
-        int maxX = driver.manage().window().getSize().getWidth();
-        int maxY = driver.manage().window().getSize().getHeight();
-        int startX = 0, startY = 0, endX = 0, endY = 0;
-      //Swipe towards up
-        startX = (int) (maxX * 0.5);
-        startY = (int) (maxY * 0.8);
-        endX = (int) (maxX * 0.5);
-        endY = (int) (maxY * 0.2);
-        PointerInput input1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
-        Sequence swipeUp = new Sequence(input1, 1);
-        swipeUp.addAction(input1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
-        swipeUp.addAction(input1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-        swipeUp.addAction(input1.createPointerMove(Duration.ofMillis(2000), PointerInput.Origin.viewport(), endX, endY));
-        swipeUp.addAction(input1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-        driver.perform(Collections.singletonList(swipeUp));
+       
+		scrollup();
+		scrollup();
+		scrollup();
+		
 		// login
 
 	
 		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"See all deals\"]")).click();
 		
-		driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"Deal: Branded Suitcase, Backpacks and Duffles - American Tourister, Safari, Skybags, etc.; \"]")).click();
+		scrollup();
+		scrollup();
 		
+		
+		driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"Deal: Branded Suitcase, Backpacks and Duffles - American Tourister, Safari, Skybags, etc.; \"]")).click();
+		Thread.sleep(5000);
+		driver.navigate().back();
 		
 		/*
 		 * driver.findElement(AppiumBy.className("android.widget.EditText")).sendKeys(
@@ -71,9 +71,30 @@ public class appTouchActions {
 		 * ("Leaf@123");
 		 * driver.findElement(AppiumBy.className("android.widget.Button")).click();
 		 */
-
+		System.out.println("the element is selected");
 		driver.quit();
 	}
+	
+	
+	public static void scrollup() {
+		 int maxX = driver.manage().window().getSize().getWidth();
+	     int maxY = driver.manage().window().getSize().getHeight();
+	     int startX = 0, startY = 0, endX = 0, endY = 0;
+	      //Swipe towards up
+	        startX = (int) (maxX * 0.5);
+	        startY = (int) (maxY * 0.85);
+	        endX = (int) (maxX * 0.5);
+	        endY = (int) (maxY * 0.16);
+	        PointerInput input1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+	        Sequence swipeUp = new Sequence(input1, 1);
+	        swipeUp.addAction(input1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
+	        swipeUp.addAction(input1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+	        swipeUp.addAction(input1.createPointerMove(Duration.ofMillis(2000), PointerInput.Origin.viewport(), endX, endY));
+	        swipeUp.addAction(input1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+	        driver.perform(Collections.singletonList(swipeUp));
+		
+	}
+	
 	
 
 
